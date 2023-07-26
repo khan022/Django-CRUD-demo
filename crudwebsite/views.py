225c070f227d6cedc59b614608f2161e194f12c9
Shafkat Khan Siam <sk.siam@outlook.com>
2023-07-26 18:22:08 +0600

    database connected with manual record

--- crudwebsite/views.py
+++ crudwebsite/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -22,7 +25,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
